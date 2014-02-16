53296997e73e2c8218d5a4c4e0557a35dffff83e
Kyle Marek-Spartz <kyle.marek.spartz@gmail.com>
2014-02-15 21:25:44 -0600

    Whitespace

--- src/Language/TaPL/TypedBoolean/Types.hs
+++ src/Language/TaPL/TypedBoolean/Types.hs
@@ -10,5 +10,7 @@ data Ty = TyBool
 typeOf :: Term -> Maybe Ty
 typeOf TmTrue = Just TyBool
 typeOf TmFalse = Just TyBool
+
 typeOf (TmIf t1 t2 t3) | typeOf t1 == Just TyBool && typeOf t2 == typeOf t3 = typeOf t2
+
 typeOf _ = Nothing
